cb7f199b693068dbeed88cbfd51768a6499d9183
Hema <hema@localhost.localdomain>
2026-03-10 20:58:23 +0100

    success

--- forkcheck.c
+++ forkcheck.c
@@ -1,1 +1,27 @@
-//C Program to create new process using Linux API system calls fork() and exit()
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if(pid > 0)   // Parent process
+    {
+        printf("I am parent, my PID is %d\n", getpid());
+        sleep(10);   // keep parent alive to see zombie in ps
+    }
+    else if(pid == 0)   // Child process
+    {
+        printf("I am child, my PID is %d\n", getpid());
+        printf("My parent PID is: %d\n", getppid());
+    }
+    else
+    {
+        printf("Fork failed\n");
+    }
+
+    return 0;
+}
